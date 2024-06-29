c2060b20b551f65833b8ce3f21ffc467b0e13fd9
Nathan Wendt <nathanswendt@gmail.com>
2024-06-29 12:13:38 -0700

    image scaling

--- src/pages/index.js
+++ src/pages/index.js
@@ -40,7 +40,12 @@ export default function Home() {
               </div>
             </div>
             <div className='w-1/2'>
-              <Image src={profilePic} alt="NathanWendt" className='w-full h-auto' />
+              <Image src={profilePic} alt="NathanWendt" className='w-full h-auto'
+              priority
+              sizes="(max-width: 768px) 100vw,
+              (max-width: 1200px) 50vw,
+              50vw"
+              />
             </div>
           </div>
         </Layout>
